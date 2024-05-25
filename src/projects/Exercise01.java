3e6fb2e4e1f9985ce38e562d1a5df00d5a3d3771
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-05-25 08:45:56 +0200

    Initial commit2

--- src/projects/Exercise01.java
+++ src/projects/Exercise01.java
@@ -2,6 +2,6 @@ package projects;
 
 public class Exercise01 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello worlds");
     }
 }
